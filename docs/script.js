1084acf8afcc6d9e555ae82df93b89794afa8521
Dhubu <dhubuvishnu@example.com>
2026-08-10 19:34:38 +0530

    new branch

--- docs/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('downloadBtn').addEventListener('click', function() {
-    // Replace with your actual file URL
-    const fileUrl = 'DO-NOT-DELETE-ASSETS.zip';
-    const link = document.createElement('a');
-    link.href = fileUrl;
-    link.download = 'DO-NOT-DELETE-ASSETS.zip';
-    link.click();
-});
